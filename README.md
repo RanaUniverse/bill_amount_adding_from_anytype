c5be0ea2397a250c106cfbd94f9ca136a553891f
RanaUniverse <RanaUniverse321@gmail.com>
2025-06-12 22:00:12 +0530

    I finally make one logic to sum all the values from the copy paste. also added the logic in the readme file

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ The Below copy > then i need to paste those so that i can get the total.
 After the Copy This looks like.
 
 ![Copy values](./values_paste.png)
+
+# Logic To Make This code
+
+1. First i need to make a List of The valeus.
+2. I need to remove the `₹` sign from the items of the list.
+3. I need to convert to int so that i can use addition (+) to this list.
+4. I will get the sum value using the build in `sum()`.
--- main.py
+++ main.py
@@ -1,4 +1,36 @@
 """
 I will make this script which will add easily
 My bills values in total.
+The input value can be the type of input value_variable i passed here.
 """
+
+input_value = """
+₹405
+₹90
+₹40
+₹25
+₹60  ₹25
+₹10
+"""
+
+print(input_value)
+
+
+normal_list = input_value.split()
+print("Input Value's List is:", normal_list)
+
+str_value_list: list[str] = []
+for i in normal_list:
+    i_value_only_str = i.removeprefix("₹")
+    str_value_list.append(i_value_only_str)
+print(str_value_list)
+
+
+int_value_list: list[int] = []
+for i in str_value_list:
+    i_as_int = int(i)
+    int_value_list.append(i_as_int)
+print(int_value_list)
+
+total_value = sum(int_value_list)
+print(f"The Total Sum of {len(int_value_list)} Items is,", f"₹{total_value}")
